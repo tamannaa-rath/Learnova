1c52b4b058b23903856744448ab65d82afbf90c6
Aditya8369 <mahajan2005aditya@gmail.com>
2026-05-28 20:05:22 +0530

    fixes

--- components/ui/DashboardSkeleton.js
+++ components/ui/DashboardSkeleton.js
@@ -176,14 +176,6 @@ const DashboardSkeleton = () => {
         </div>
       </div>
 
-      {/* Shimmer Animation */}
-      <style jsx>{`
-        @keyframes shimmer {
-          100% {
-            transform: translateX(100%);
-          }
-        }
-      `}</style>
     </div>
   );
 };
